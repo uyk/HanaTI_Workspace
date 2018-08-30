32f7e8b631cbaae0756547bc57d61069d570ec45
kosta\uyk <meopulior@gmail.com>
2018-08-30 12:24:39 +0900

    UserFrame에 MouseListener 올리기

--- AWTStudy/src/GridBagLayoutPanel.java
+++ AWTStudy/src/GridBagLayoutPanel.java
@@ -7,20 +7,22 @@ import java.awt.GridBagLayout;
 import java.awt.Panel;
 
 public class GridBagLayoutPanel extends Panel {
-	Button button1, button2;
+	Button button1, button2, button3;
 	GridBagLayout gridBagLayout;
 	GridBagConstraints gridBagConstraints;
 	
 	public GridBagLayoutPanel() {
 		button1 = new Button("Button 1");
 		button2 = new Button("Button 2");
+		button3 = new Button("Button 3");
 		gridBagLayout = new GridBagLayout();
 		gridBagConstraints = new GridBagConstraints();
 	}
 	public void setContents() {
 		setLayout(gridBagLayout);
 		add(button1, 0, 0, 1, 1, 0, 0);
-		add(button2, 1, 0, 1, 1, 1, 1);
+		add(button2, 1, 0, 1, 1, 1, 0);
+		add(button3, 0, 1, 2, 1, 1, 1);
 
 	}
 	
@@ -34,7 +36,7 @@ public class GridBagLayoutPanel extends Panel {
 		gridBagConstraints.weightx = weightx;
 		gridBagConstraints.weighty = weighty;
 
-		gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;	// 호리즌탈(가로채움), 버티컬(새로채움), ?(전체채움)
+		gridBagConstraints.fill = GridBagConstraints.BOTH;	// 호리즌탈(가로채움), 버티컬(새로채움), BOTH(전체채움), NONE(디폴트)
 		//gridBagConstraints.insets = new Insets(5, 5, 5, 5);			//마진
 		gridBagLayout.setConstraints(component, gridBagConstraints);
 		
@@ -49,7 +51,8 @@ public class GridBagLayoutPanel extends Panel {
 		panel.setContents();
 		
 		frame.add(panel);
-		frame.setSize(800,800);
+//		frame.setSize(800,800);
+		frame.pack();
 		frame.setVisible(true);
 		
 	}
--- AWTStudy/src/UserFrame.java
+++ AWTStudy/src/UserFrame.java
@@ -1,15 +1,20 @@
 import java.awt.BorderLayout;
 import java.awt.Button;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Random;
 
-public class UserFrame extends Frame {
+public class UserFrame extends Frame implements MouseListener {
 	Button nButton, eButton, wButton, sButton, cButton;
 
 	public UserFrame() {
 		this("이름없음");
 	}
+	
 	public UserFrame(String title) {
 		super(title);
 		this.eButton = new Button("EAST");
@@ -18,6 +23,7 @@ public class UserFrame extends Frame {
 		this.nButton = new Button("NORTH");
 		this.cButton = new Button("CENTER");
 	}
+	
 	// 화면 배치
 	public void setContents() {
 		// 레이아웃매니저 교체
@@ -29,9 +35,56 @@ public class UserFrame extends Frame {
 		add(cButton, BorderLayout.CENTER);
 	}
 	
+	public void eventRegist() {
+		eButton.addMouseListener(this);
+	}
+
+	
+	// MouseListener 인터페이스의 추상메소드 구현
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("마우스 클릭되었습니다.");
+		Object eventSource = e.getSource();
+		if(eventSource instanceof Button) {
+			Button button = (Button)eventSource;
+			Random random = new Random();
+			int r = random.nextInt(255);
+			int g = random.nextInt(255);
+			int b = random.nextInt(255);
+			button.setBackground(new Color(r, g, b));
+			System.out.println("r : " + r + ", g : "+ g + ", b : "+ b);
+		
+		}
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
 	public static void main(String[] args) {
 		UserFrame frame = new UserFrame("User Frame Title");
 		frame.setContents();
+		frame.eventRegist();
+		
 		frame.setSize(800, 600);
 		frame.setVisible(true);
 	}
