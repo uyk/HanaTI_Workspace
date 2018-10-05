285e96c1a2768652cbb55ba8a865f0c416077305
kosta\uyk <meopulior@gmail.com>
2018-10-05 16:51:33 +0900

    마이너스 어카운트 까지. 부모클래스의 메소드를 사용하려면?

--- Project_WEB_AMS/WebContent/js/Account.js
+++ Project_WEB_AMS/WebContent/js/Account.js
@@ -1,3 +1,37 @@
 /**
- * 
+ *  계좌 정보를 저장하는 Account 생성자
  */
+
+// 생성자
+function Account(num,owner,pw,rest) {
+	this.num = num;
+	this.owner = owner;
+	this.pw = pw;
+	this.rest = rest;
+}
+
+// Account의 모든 인스턴스가 공유하는 변수
+Account.bankName = "KOSTA 은행";
+
+// Account의 prototype에 메소드 선언
+// 입금시 money만큼 잔액이 증가하는 메소드
+Account.prototype.deposit = function(money) {
+	if(money <= 0) return false;
+	this.rest += money;
+	return this.rest;
+}
+// 출금시 money만큼 잔액이 감소하는 메소드
+Account.prototype.withdraw = function(money) {
+	if(money <= 0) return false;
+	if(money > this.rest) return false;
+	this.rest -= money;
+	return this.rest;
+}
+// 패스워드를 검사하는 메소드
+Account.prototype.checkPasswd = function (pw) {
+	return this.pw == pw;
+}
+// 계좌 정보를 문자열로 반환하는 메소드
+Account.prototype.toString = function() {
+	return Account.bankName + " " + this.num + " " + this.owner + " " + this.pw + " " + this.rest;
+}
--- Project_WEB_AMS/WebContent/js/MinusAccount.js
+++ Project_WEB_AMS/WebContent/js/MinusAccount.js
@@ -1,3 +1,21 @@
 /**
- * 
+ * Account 생성자를 상속받아 마이너스 계좌 정보를 저장하는 생성자. 
  */
+
+function MinusAccount(num,owner,pw,rest, bm) {
+	Account.call(this, num, owner, pw, rest);
+	this.bm = bm;
+}
+MinusAccount.prototype = new Account();
+delete MinusAccount.prototype.num;
+delete MinusAccount.prototype.owner;
+delete MinusAccount.prototype.pw;
+delete MinusAccount.prototype.rest;
+MinusAccount.prototype.constructor = MinusAccount;
+
+// 마이너스 계좌의 내용을 반환하는 메소드
+MinusAccount.prototype.toString = function() {
+	console.log("q");
+
+	return Account.bankName + " " + this.num + " " + this.owner + " " + this.pw + " " + this.rest + " " + this.bm;
+}
