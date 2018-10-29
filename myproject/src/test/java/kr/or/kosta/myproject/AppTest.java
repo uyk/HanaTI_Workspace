08ba8de5de8bbfd27f10bd0aeca3045566f516bf
kosta\uyk <meopulior@gmail.com>
2018-10-29 17:35:27 +0900

    메이븐 웹 프로젝트 생성 및 xml 설정, 서버 설정

--- myproject/src/test/java/kr/or/kosta/myproject/AppTest.java
+++ myproject/src/test/java/kr/or/kosta/myproject/AppTest.java
@@ -1,38 +1,25 @@
 package kr.or.kosta.myproject;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	@Before
+	public void init() {
+		
+	}
+	
+	@After
+	public void finish() {
+		
+	}
+	
+	@Test
+	public void testApp() {
+		System.out.println("메이븐");
+	}
 }
